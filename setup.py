3382019a6a8ed3a095465637729a1c89ea8c632f
Cory Zue <czue@dimagi.com>
2019-06-18 13:48:48 +0200

    bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='celery-progress',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
