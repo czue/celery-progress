cdaee881d6455322bddce62ca660a3c65b09edb9
Cory Zue <czue@dimagi.com>
2020-05-07 13:37:04 +0200

    bump version
    this is actually out of sequence, had this change locally (facepalm)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='celery-progress',
-    version='0.0.8',
+    version='0.0.9',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
