0ca5e10b189f94d2acfacfe246cf730cdeffd43b
Omar Khashoggi <omar.w.kh@gmail.com>
2020-08-21 16:52:57 +0300

    handle network and http errors in the polling client

--- celery_progress/static/celery_progress/celery_progress.js
+++ celery_progress/static/celery_progress/celery_progress.js
@@ -22,7 +22,7 @@ var CeleryProgressBar = (function () {
         progressBarMessageElement.innerHTML = progress.current + ' of ' + progress.total + ' processed. ' + description;
     }
 
-    function updateProgress (progressUrl, options) {
+    async function updateProgress (progressUrl, options) {
         options = options || {};
         var progressBarId = options.progressBarId || 'progress-bar';
         var progressBarMessage = options.progressBarMessageId || 'progress-bar-message';
@@ -31,13 +31,22 @@ var CeleryProgressBar = (function () {
         var onProgress = options.onProgress || onProgressDefault;
         var onSuccess = options.onSuccess || onSuccessDefault;
         var onError = options.onError || onErrorDefault;
+        var onNetworkError = options.onNetworkError || onErrorDefault;
+        var onHttpError = options.onHttpError || onErrorDefault;
         var pollInterval = options.pollInterval || 500;
         var resultElementId = options.resultElementId || 'celery-result';
         var resultElement = options.resultElement || document.getElementById(resultElementId);
         var onResult = options.onResult || onResultDefault;
 
 
-        fetch(progressUrl).then(function(response) {
+        let response;
+        try {
+            response = await fetch(progressUrl);
+        } catch (networkError) {
+            onNetworkError(progressBarElement, progressBarMessageElement, "Network Error");
+        }
+
+        if (response.status === 200) {
             response.json().then(function(data) {
                 if (data.progress) {
                     onProgress(progressBarElement, progressBarMessageElement, data.progress);
@@ -55,12 +64,16 @@ var CeleryProgressBar = (function () {
                     }
                 }
             });
-        });
+        } else {
+            onHttpError(progressBarElement, progressBarMessageElement, "HTTP Code " + response.status);
+        }
     }
     return {
         onSuccessDefault: onSuccessDefault,
         onResultDefault: onResultDefault,
         onErrorDefault: onErrorDefault,
+        onNetworkError: onErrorDefault,
+        onHttpError: onErrorDefault,
         onProgressDefault: onProgressDefault,
         updateProgress: updateProgress,
         initProgressBar: updateProgress,  // just for api cleanliness
