aa2571856e08e39050fb2b0550aae78ac93049cd
Cory Zue <czue@dimagi.com>
2020-08-27 09:55:15 +0200

    update setup.py and bump version

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
 import os
 from setuptools import find_packages, setup
 
-# convert md to rst for pypi https://stackoverflow.com/a/23265673/8207
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
+from glob import glob
 
 readme_name = os.path.join(os.path.dirname(__file__), 'README.md')
+
+with open(readme_name, 'r') as readme:
+    long_description = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='celery-progress',
-    version='0.0.10',
+    version='0.0.11',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='Drop in, configurable, dependency-free progress bars for your Django/Celery applications.',
-    long_description=read_md(readme_name),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/czue/celery-progress',
     author='Cory Zue',
     author_email='cory@coryzue.com',
@@ -38,6 +36,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    data_files=[
+        ('static/celery_progress', glob('celery_progress/static/celery_progress/*', recursive=True)),
     ],
     extras_require={
         'websockets': ['channels'],
