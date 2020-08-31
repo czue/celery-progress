dd4e742e4e1ffdbf63145353897d2cdf3eba0f9b
Omar Khashoggi <omar.w.kh@gmail.com>
2020-08-31 21:08:55 +0300

    make the ws client a subclass of the http client

--- celery_progress/static/celery_progress/websockets.js
+++ celery_progress/static/celery_progress/websockets.js
@@ -1,67 +1,32 @@
-var CeleryWebSocketProgressBar = (function () {
-    function onSuccessDefault(progressBarElement, progressBarMessageElement, result) {
-        CeleryProgressBar.onSuccessDefault(progressBarElement, progressBarMessageElement);
+class CeleryWebSocketProgressBar extends CeleryProgressBar {
+
+    constructor(progressUrl, options) {
+        super(progressUrl, options);
     }
 
-    function onResultDefault(resultElement, result) {
-        CeleryProgressBar.onResultDefault(resultElement, result);
-    }
-
-    function onErrorDefault(progressBarElement, progressBarMessageElement, excMessage, data) {
-        CeleryProgressBar.onErrorDefault(progressBarElement, progressBarMessageElement, excMessage, data);
-    }
-
-    function onProgressDefault(progressBarElement, progressBarMessageElement, progress) {
-        CeleryProgressBar.onProgressDefault(progressBarElement, progressBarMessageElement, progress);
-    }
-
-    function onData(data, onProgress, onSuccess, onTaskError, onDataError, onResult, progressBarElement, progressBarMessageElement, resultElement) {
-        return CeleryProgressBar.onData(data, onProgress, onSuccess, onTaskError, onDataError, onResult, progressBarElement, progressBarMessageElement, resultElement);
-    }
-
-    function initProgress (progressUrl, options) {
-        options = options || {};
-        var progressBarId = options.progressBarId || 'progress-bar';
-        var progressBarMessage = options.progressBarMessageId || 'progress-bar-message';
-        var progressBarElement = options.progressBarElement || document.getElementById(progressBarId);
-        var progressBarMessageElement = options.progressBarMessageElement || document.getElementById(progressBarMessage);
-        var onProgress = options.onProgress || onProgressDefault;
-        var onSuccess = options.onSuccess || onSuccessDefault;
-        var onError = options.onError || onErrorDefault;
-        var onDataError = options.onDataError || onError;
-        var onTaskError = options.onTaskError || onError;
-        var resultElementId = options.resultElementId || 'celery-result';
-        var resultElement = options.resultElement || document.getElementById(resultElementId);
-        var onResult = options.onResult || onResultDefault;
-
+    async connect() {
         var ProgressSocket = new WebSocket((location.protocol === 'https:' ? 'wss' : 'ws') + '://' +
-            window.location.host + progressUrl);
+            window.location.host + this.progressUrl);
 
         ProgressSocket.onopen = function (event) {
             ProgressSocket.send(JSON.stringify({'type': 'check_task_completion'}));
         };
 
+        const bar = this;
         ProgressSocket.onmessage = function (event) {
             let data;
             try {
                 data = JSON.parse(event.data);
             } catch (parsingError) {
-                onDataError(progressBarElement, progressBarMessageElement, "Parsing Error")
+                bar.onDataError(bar.progressBarElement, bar.progressBarMessageElement, "Parsing Error")
                 throw parsingError;
             }
 
-            const done = onData(data, onProgress, onSuccess, onTaskError, onDataError, onResult, progressBarElement, progressBarMessageElement, resultElement);
+            const complete = bar.onData(data);
 
-            if (done === true || done === undefined) {
+            if (complete === true || complete === undefined) {
                 ProgressSocket.close();
             }
-        }
+        };
     }
-    return {
-        onSuccessDefault: onSuccessDefault,
-        onResultDefault: onResultDefault,
-        onErrorDefault: onErrorDefault,
-        onProgressDefault: onProgressDefault,
-        initProgressBar: initProgress,
-    };
-})();
+}
