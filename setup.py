612880a4dcbd7696c31d954f51b7632409d8696e
Cory Zue <czue@dimagi.com>
2020-10-10 14:13:49 +0200

    bump version to 0.0.13

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='celery-progress',
-    version='0.0.12',
+    version='0.0.13',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
