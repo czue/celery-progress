f02f466c233343124aa27bbf69f7812f9627243c
Ethan <ethan.harriz@gmail.com>
2020-10-28 00:41:24 -0400

    Add IGNORED result state to Progress.get_info()

--- celery_progress/backend.py
+++ celery_progress/backend.py
@@ -82,6 +82,12 @@ class Progress(object):
                 'progress': _get_completed_progress(),
                 'result': result,
             })
+        elif self.result.state == 'IGNORED':
+            response.update({
+                'complete': True,
+                'success': None,
+                'progress': _get_completed_progress()
+            })
         elif self.result.state == PROGRESS_STATE:
             response.update({
                 'complete': False,
