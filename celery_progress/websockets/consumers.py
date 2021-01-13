692473cd485ecb6905c13027ea6cccaa306b49e1
Aydin Salehi <aidinsalehi2008@gmail.com>
2021-01-13 13:14:31 +0330

    update sync_to_async to database_sync_to_async (https://channels.readthedocs.io/en/stable/topics/databases.html)

--- celery_progress/websockets/consumers.py
+++ celery_progress/websockets/consumers.py
@@ -27,12 +27,12 @@ class ProgressConsumer(AsyncWebsocketConsumer):
         task_type = text_data_json['type']
 
         if task_type == 'check_task_completion':
-            from asgiref.sync import sync_to_async
+            from channels.db import database_sync_to_async
             await self.channel_layer.group_send(
                 self.task_id,
                 {
                     'type': 'update_task_progress',
-                    'data': await sync_to_async(Progress(AsyncResult(self.task_id)).get_info, thread_sensitive=True)()
+                    'data': await database_sync_to_async(Progress(AsyncResult(self.task_id)).get_info)()
                 }
             )
 
