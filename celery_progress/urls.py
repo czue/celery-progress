3ee02177af5a29502cc0dbe6c375441587b39c64
abedyngash <abedyngash@gmail.com>
2021-03-03 15:50:21 +0300

    Changed django.conf.urls.url to django.urls.re_path in respect to latest Django release : RemovedInDjango40Warning

--- celery_progress/urls.py
+++ celery_progress/urls.py
@@ -1,7 +1,7 @@
-from django.urls import repath
+from django.urls import re_path
 from . import views
 
 app_name = 'celery_progress'
 urlpatterns = [
-    repath(r'^(?P<task_id>[\w-]+)/$', views.get_progress, name='task_status')
+    re_path(r'^(?P<task_id>[\w-]+)/$', views.get_progress, name='task_status')
 ]
