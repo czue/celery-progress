a7ea6a3053c99f826a1db3c1ba2444266cf28b8f
Cory Zue <cory@coryzue.com>
2022-01-25 09:04:30 +0200

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='celery-progress',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
