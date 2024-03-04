c7efa1d0909677db55763160329b2069eb2d9cf4
Cory Zue <cory@coryzue.com>
2024-03-04 11:31:14 +0200

    proof of concept for group results

--- celery_progress/urls.py
+++ celery_progress/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'celery_progress'
 urlpatterns = [
-    re_path(r'^(?P<task_id>[\w-]+)/$', views.get_progress, name='task_status')
+    re_path(r'^(?P<task_id>[\w-]+)/$', views.get_progress, name='task_status'),
+    re_path(r'^g/(?P<group_id>[\w-]+)/$', views.get_group_progress, name='group_status')
 ]
